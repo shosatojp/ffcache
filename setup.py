8ca9f5b9115910a1188f53bd0df7d0211b2f1003
shosatojp <me@shosato.jp>
2020-10-08 11:52:12 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 import glob
 import subprocess
 from setuptools import setup
-from setuptools.command.install import install
 from setuptools.command.build_ext import build_ext
 from setuptools.extension import Extension
 import sysconfig
@@ -10,23 +9,13 @@ import os
 import shutil
 
 
-class MakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
 class my_ext(build_ext):
     def build_extension(self, ext):
         subprocess.run(['make', f'-j{os.cpu_count()}'])
-        binname = '_ffcache.cpython-38-x86_64-linux-gnu.so'
-        outpath = os.path.join(self.build_lib, binname)
-        shutil.move(binname, outpath)
-
-
-# class my_install(install):
-#     def get_outputs(self):
-#         return install.get_outputs(self) #+ ['_ffcache.cpython-38-x86_64-linux-gnu.so']
+        bins = glob.glob('*.so')
+        for bin in bins:
+            outpath = os.path.join(self.build_lib, bin)
+            shutil.move(bin, outpath)
 
 
 with open('README.md', 'rt', encoding='utf-8') as fh:
@@ -41,22 +30,23 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/shosatojp/ffcache',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(),  # find package `ffcache`
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    scripts=[],
     install_requires=[
         'pybind11',
-        'brotli'
+        'brotli',
     ],
     cmdclass={
-        'build_ext': my_ext
+        # custom build script
+        'build_ext': my_ext,
     },
     ext_modules=[
-        MakeExtension('_ffcache2'),
+        # for c++ extension (arg `_ffcache` is arbitrary, set `sourcedir` to include all files in this directory)
+        Extension('_ffcache', [], sourcedir=os.path.abspath('.')),
     ]
 )
